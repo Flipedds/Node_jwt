da0d36fbdd5a1fdf120cb833ec531e16a2ef0ef5
Filipe André <flipeandre12@gmail.com>
2023-07-19 18:06:14 -0300

    feat: adiciona nova rota

---
+++ Controllers/GetUserById.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+const checkToken = require('../Middlewares/checkToken');
+
+// Private rote
+router.get('/:id', checkToken, async (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    // consulta sem a senha
+    const user = await User.findById(id, '-password');
+
+    if (!user){
+        return res.status(404).json({msg: "Usuário não encontrado"});
+    }
+
+    return res.status(200).json({user});
+
+});
+
+module.exports = router;
