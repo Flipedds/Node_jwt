e4c398f8a9593964399924432e09f393f9a596e1
Filipe André <flipeandre12@gmail.com>
2023-07-21 23:01:04 -0300

    feat: Adiciona classe App.

--- app.js
+++ app.js
@@ -1,29 +1,4 @@
-const express = require('express');
-const Auth = require('./Routes/Auth');
-const Register = require('./Routes/Resgister');
-const GetUserById = require('./Routes/GetUserById');
 const Database = require('./Database/Mongo');
-
-class App {
-  constructor() {
-    this.app = express();
-    this.middlewares();
-    this.routes();
-    this.app.listen(3000);
-  }
-
-  middlewares() {
-    this.app.use(express.json());
-  }
-
-  routes() {
-    this.app.get('/', (req, res) =>{
-        res.status(200).json({msg: "Bem vindo a nossa api"});
-    });
-    this.app.use('/user', GetUserById);
-    this.app.use('/auth/register', Register);
-    this.app.use('/auth/login', Auth);
-  }
-}
+const App = require('./models/App');
 
 new App();
---
+++ models/App.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const Auth = require('../Routes/Auth');
+const Register = require('../Routes/Resgister');
+const GetUserById = require('../Routes/GetUserById');
+
+class App {
+    constructor() {
+      this.app = express();
+      this.middlewares();
+      this.routes();
+      this.app.listen(3000);
+    }
+  
+    middlewares() {
+      this.app.use(express.json());
+    }
+  
+    routes() {
+      this.app.get('/', (req, res) =>{
+          res.status(200).json({msg: "Bem vindo a nossa api"});
+      });
+      this.app.use('/user', GetUserById);
+      this.app.use('/auth/register', Register);
+      this.app.use('/auth/login', Auth);
+    }
+  }
+
+module.exports = App;
